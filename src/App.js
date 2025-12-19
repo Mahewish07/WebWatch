1b7fba6dd557178acdd538737d027d050015082b
Mahewish07 <mahewishkhan1703@gmail.com>
2025-12-19 11:34:04 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
-import Home from './Home';
 import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Home from './Home';
 import Broadcast from './Broadcast';
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Home Page */}
         <Route path="/" element={<Home />} />
+        {/* Login Page */}
         <Route path="/login" element={<Login />} />
+        
+        {/* Signup Page */}
         <Route path="/signup" element={<Signup />} />
+        
+        {/* Dashboard Page */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+        {/* Camera Broadcast Page */}
         <Route path="/broadcast" element={<Broadcast />} />
       </Routes>
     </Router>
@@ -19,3 +29,6 @@ function App() {
 }
 
 export default App;
+
+
+
